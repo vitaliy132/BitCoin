13041ee71beb5a31877e2dfc844fbe68f45c67dd
vitaliy132 <vitaliymed060@gmail.com>
2025-01-27 19:26:19 +0000

    added refresh interval

--- App.jsx
+++ App.jsx
@@ -1,43 +1,47 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-function App() {
-  const [portfolio, setPortfolio] = useState([]);
+const App = () => {
+  const [portfolio, setPortfolio] = useState({});
+  const refreshInterval = 5000;
+
+  const fetchExchangeRates = async () => {
+    const assets = ["BTC", "ETH", "XRP"];
+    try {
+      const promises = assets.map((asset) =>
+        axios.get(
+          `https://rest.coinapi.io/v1/exchangerate/${asset}/USD?apikey=c7792603-9a9a-4a59-8bd6-e3335c2fd98f`,
+        ),
+      );
+      const responses = await Promise.all(promises);
+      const exchangeRates = responses.reduce((acc, response, index) => {
+        acc[assets[index]] = response.data.rate;
+        return acc;
+      }, {});
+      setPortfolio(exchangeRates);
+    } catch (error) {
+      console.error("Error fetching exchange rates:", error);
+    }
+  };
 
   useEffect(() => {
-    async function fetchExchangeRates() {
-      try {
-        const assets = ["BTC", "ETH", "XRP"];
-        const promises = assets.map((asset) =>
-          axios.get(
-            `https://rest.coinapi.io/v1/exchangerate/${asset}/USD?apikey=c7792603-9a9a-4a59-8bd6-e3335c2fd98f`,
-          ),
-        );
-        const responses = await Promise.all(promises);
-        const exchangeRates = responses.reduce((acc, response, index) => {
-          acc[assets[index]] = response.data.rate;
-          return acc;
-        }, {});
-        setPortfolio(exchangeRates);
-      } catch (error) {
-        console.error("Error fetching exchange rates:", error);
-      }
-    }
     fetchExchangeRates();
-  }, []);
+    const intervalId = setInterval(fetchExchangeRates, refreshInterval);
 
+    return () => clearInterval(intervalId);
+  }, []);
   return (
     <div className="App">
-      <h1>CoinAPI Cryptocurrency Portfolio Tracker App</h1>
+      <h1>CoinAPI Cryptocurrency Portfolio Tracker</h1>
       <ul>
         {Object.entries(portfolio).map(([asset, exchangeRate]) => (
           <li key={asset}>
-            {asset}: {exchangeRate.toFixed(2)} USD
+            {asset}: {exchangeRate ? exchangeRate.toFixed(2) : "Loading..."} USD
           </li>
         ))}
       </ul>
     </div>
   );
-}
+};
 
 export default App;
