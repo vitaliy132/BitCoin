bbbd4d333832c8df5a8f4fc02c3a774e1f49cdec
vitaliy132 <vitaliymed060@gmail.com>
2025-02-18 16:57:12 +0000

    131

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import News from "./components/News";
 import Portfolio from "./components/Portfolio";
 import BitcoinPricePrediction from "./components/BitcoinYearPrice";
+import "./App.css";
 
 const Navbar = () => {
   return (
